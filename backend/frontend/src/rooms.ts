ffdba7bfc1f4c472e42826a5e8779df7af2902c3
Ónodi Miklós <mikonodi33@gmail.com>
2026-04-10 16:40:24 +0200

    Continued rooms. Now: Clone to themes, learn

--- backend/frontend/src/rooms.ts
+++ backend/frontend/src/rooms.ts
@@ -5,28 +5,55 @@ const closeBtn: HTMLElement = document.getElementById(
   "closeBtn",
 ) as HTMLButtonElement;
 
-if (createCard) {
-  createCard.onclick = show;
-}
-if (closeBtn) {
-  closeBtn.onclick = hide;
-}
-
 const cover: HTMLElement = document.getElementById("cover") as HTMLDivElement;
 const createRoom: HTMLElement = document.getElementById(
   "createRoom",
 ) as HTMLDivElement;
 
-function show(): void {
-  if (cover && createRoom) {
-    cover.style.display = "block";
-    createRoom.style.display = "flex";
-  }
+if (createCard) {
+  createCard.onclick = function (): void {
+    if (cover && createRoom) {
+      cover.style.display = "block";
+      createRoom.style.display = "flex";
+    }
+  };
+}
+if (closeBtn) {
+  closeBtn.onclick = function (): void {
+    if (cover && createRoom) {
+      cover.style.display = "none";
+      createRoom.style.display = "none";
+    }
+  };
 }
 
-function hide(): void {
-  if (cover && createRoom) {
-    cover.style.display = "none";
-    createRoom.style.display = "none";
+const checkbox: HTMLInputElement = document.getElementById(
+  "private",
+) as HTMLInputElement;
+
+const jelkodHolder: HTMLDivElement = document.getElementById(
+  "jelkodHolder",
+) as HTMLDivElement;
+
+if (checkbox && jelkodHolder) {
+  checkbox.onclick = check;
+}
+function check(): void {
+  if (checkbox.checked) {
+    jelkodHolder.style.visibility = "visible";
+  } else {
+    jelkodHolder.style.visibility = "hidden";
   }
 }
+check();
+
+interface User {
+  nev: string;
+  haj: number;
+  arc: number;
+  szin: number;
+}
+const userJson = localStorage.getItem("user");
+const user: User = userJson
+  ? JSON.parse(userJson)
+  : { nev: "", haj: 0, arc: 0, szin: 0 };
